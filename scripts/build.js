c8306adbafe9923a384d515cbbe744cf8b9d23c7
bendytree <josh@bendytree.com>
2013-04-26 11:08:52 -0500

    added auto versioning

--- scripts/build.js
+++ scripts/build.js
@@ -65,13 +65,29 @@ async.series([
         });
     },
     function(callback){
+        //save the raw js in the libs folder
         fs.writeFile("./lib/popple.js", js, function(err) {
             if(err) console.log(err);
             callback();
         }); 
     },
     function(callback){
+        //save the compressed js in the libs folder
         fs.writeFile("./lib/popple.min.js", minjs, function(err) {
+            if(err) console.log(err);
+            callback();
+        }); 
+    },
+    function(callback){
+        //bump the version
+        var package = require('../package.json');
+        var v = package.version;
+        var i = v.lastIndexOf('.');
+        package.version = v.substring(0, i) + '.' + (parseInt(v.substring(i+1))+1);
+        console.log();
+        console.log('new version: '+package.version);
+        var json = JSON.stringify(package, null, 2);
+        fs.writeFile("./package.json", json, function(err) {
             if(err) console.log(err);
             callback();
         }); 
